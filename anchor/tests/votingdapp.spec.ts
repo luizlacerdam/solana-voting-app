8934ce67ab8fe1ed3b59993c577b7ab3678984b2
luizlacerdam <luizlacerdam.dev@gmail.com>
2025-01-03 12:13:15 -0500

    initializePoll test-completed

--- anchor/tests/votingdapp.spec.ts
+++ anchor/tests/votingdapp.spec.ts
@@ -10,7 +10,7 @@ describe('Testing Voting App', () => {
 
   const votingAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
-  it('Initialize Votingdapp', async () => {
+  it('Initialize Poll', async () => {
     const context = await startAnchor("", [{name: "votingdapp", programId: votingAddress}], [])
     const provider = new BankrunProvider(context);
    
@@ -25,6 +25,22 @@ describe('Testing Voting App', () => {
       new anchor.BN(0),
       new anchor.BN(1835922190),
     ).rpc();
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8)],
+      votingAddress,
+    )
+
+    // fetchs poll
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log(poll);
+
+    expect(poll.pollId.toNumber()).toEqual(1);
+    expect(poll.description).toEqual("What is favorite type or penut butter?");
+    expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
+    expect(poll.candidateAmount.toNumber()).toEqual(0);
+    
 
   }, 30000)
 
